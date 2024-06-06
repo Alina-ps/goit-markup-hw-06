b09c8d8520bd7dbc37a5803b90466d9f6d99896b
Alina-ps <alinaserg2014@gmail.com>
2024-06-06 21:23:45 +0200

    did some corrections and added js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+// Elements
 const mobMenu = document.querySelector('.mob-menu');
 const openMobMenuBtn = document.querySelector('.menu-open-btn');
 const closeMobMenuBtn = document.querySelector('.menu-close-btn');
 
+const navLinks = document.querySelectorAll('.link');
+
+const backdrop = document.querySelector('.backdrop');
+const openBackdropBtn = document.querySelector('.page-button');
+const closeBackdropBtn = document.querySelector('.modal-close-btn');
+const backdropForm = document.querySelector('.modal-form');
+
+const subscribeForm = document.querySelector('.subscr-form');
+const subscribeButton = document.querySelector('.subscr-btn');
+
+//  Mobile Menu Open/Close
 const openMobileMenu = () => {
   mobMenu.classList.add('is-open');
 };
@@ -13,10 +25,18 @@ const closeMobileMenu = () => {
 openMobMenuBtn.addEventListener('click', openMobileMenu);
 closeMobMenuBtn.addEventListener('click', closeMobileMenu);
 
-const backdrop = document.querySelector('.backdrop');
-const backdropOpenButton = document.querySelector('.page-button');
-const backdropCloseButton = document.querySelector('.modal-close-btn');
+// Navigation To Sections
+const goToSection = e => {
+  closeMobileMenu();
+  const targetId = e.target.getAttribute('href').substring(1);
+  const targetSection = document.getElementById(targetId);
+};
 
+navLinks.forEach(link => {
+  link.addEventListener('click', goToSection);
+});
+
+// Backdrop Open/Close
 const openBackdrop = () => {
   backdrop.classList.add('is-open');
 };
@@ -25,5 +45,35 @@ const closeBackdrop = () => {
   backdrop.classList.remove('is-open');
 };
 
-backdropOpenButton.addEventListener('click', openBackdrop);
-backdropCloseButton.addEventListener('click', closeBackdrop);
+openBackdropBtn.addEventListener('click', openBackdrop);
+closeBackdropBtn.addEventListener('click', closeBackdrop);
+
+// Backdrop Form Submission Handler
+const sendContacts = e => {
+  e.preventDefault();
+
+  const userName = document.getElementById('user-name').value;
+  const userPhone = document.getElementById('user-tel').value;
+  const userEmail = document.getElementById('user-email').value;
+  const userComment = document.getElementById('user-comment').value;
+
+  console.log(
+    `Name: ${userName}, Phone: ${userPhone}, Email: ${userEmail}, Comment: ${userComment}`
+  );
+
+  backdropForm.reset();
+};
+
+backdropForm.addEventListener('submit', sendContacts);
+
+// Subscription Form Submission Handler
+const subscribeUser = e => {
+  e.preventDefault();
+
+  const email = subscribeForm.querySelector('.subscr-input').value;
+  console.log(`Subscribed email: ${email}`);
+
+  subscribeForm.reset();
+};
+
+subscribeForm.addEventListener('submit', subscribeUser);
